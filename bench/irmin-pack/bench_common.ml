d79476278f9bd6e4fe6dd4b974ba5db6e71021dc
icristescu <domninac@gmail.com>
2021-05-03 22:43:40 +0200

    Add tree bench

--- bench/irmin-pack/bench_common.ml
+++ bench/irmin-pack/bench_common.ml
@@ -1,5 +1,20 @@
-let ( let* ) x f = Lwt.bind x f
-let ( let+ ) x f = Lwt.map f x
+(*
+ * Copyright (c) 2018-2021 Tarides <contact@tarides.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+open Lwt.Syntax
 
 let reporter ?(prefix = "") () =
   let report src level ~over k msgf =
@@ -20,14 +35,38 @@ let reporter ?(prefix = "") () =
   in
   { Logs.report }
 
+let setup_log style_renderer level =
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level level;
+  Logs.set_reporter (reporter ());
+  ()
+
 let reset_stats () =
   Index.Stats.reset_stats ();
   Irmin_pack.Stats.reset_stats ()
 
 let random_char () = char_of_int (Random.int 256)
+
 let random_string n = String.init n (fun _i -> random_char ())
-let random_blob () = random_string 10
+
+let random_blob () = random_string 10 |> Bytes.of_string
+
 let random_key () = random_string 5
+
+let default_artefacts_dir =
+  let ( / ) = Filename.concat in
+  Unix.getcwd () / "_artefacts" / Uuidm.to_string (Uuidm.v `V4)
+
+let prepare_artefacts_dir path =
+  let rec mkdir_p path =
+    if Sys.file_exists path then ()
+    else
+      let path' = Filename.dirname path in
+      if path' = path then failwith "Failed to prepare result dir";
+      mkdir_p path';
+      Unix.mkdir path 0o755
+  in
+  mkdir_p path
 
 let with_timer f =
   let t0 = Sys.time () in
@@ -35,8 +74,22 @@ let with_timer f =
   let t1 = Sys.time () -. t0 in
   (t1, a)
 
+let with_progress_bar ~message ~n ~unit =
+  let bar =
+    let w =
+      if n = 0 then 1
+      else float_of_int n |> log10 |> floor |> int_of_float |> succ
+    in
+    let pp fmt i = Format.fprintf fmt "%*Ld/%*d %s" w i w n unit in
+    let pp f = f ~width:(w + 1 + w + 1 + String.length unit) pp in
+    Progress_unix.counter ~mode:`ASCII ~width:79 ~total:(Int64.of_int n)
+      ~message ~pp ()
+  in
+  Progress_unix.with_reporters bar
+
 module Conf = struct
   let entries = 32
+
   let stable_hash = 256
 end
 
@@ -50,6 +103,7 @@ module FSHelper = struct
     try (Unix.stat f).st_size with Unix.Unix_error (Unix.ENOENT, _, _) -> 0
 
   let dict root = file (Irmin_pack.Layout.dict ~root) / 1024 / 1024
+
   let pack root = file (Irmin_pack.Layout.pack ~root) / 1024 / 1024
 
   let index root =
@@ -60,6 +114,7 @@ module FSHelper = struct
     (a + b + c) / 1024 / 1024
 
   let size root = dict root + pack root + index root
+
   let get_size root = size root
 
   let print_size_layers root =
@@ -67,19 +122,20 @@ module FSHelper = struct
     let upper1 = Filename.concat root "upper1" in
     let upper0 = Filename.concat root "upper0" in
     let lower = Filename.concat root "lower" in
-    Fmt.epr "%+04.0fus: upper1 = %d M, upper0 = %d M, lower = %d M\n%!" dt
-      (size upper1) (size upper0) (size lower)
+    Logs.app (fun l ->
+        l "%+04.0fus: upper1 = %d M, upper0 = %d M, lower = %d M\n%!" dt
+          (size upper1) (size upper0) (size lower))
 
   let rm_dir root =
     if Sys.file_exists root then (
       let cmd = Printf.sprintf "rm -rf %s" root in
-      Logs.info (fun l -> l "exec: %s\n%!" cmd);
+      Logs.info (fun l -> l "exec: %s" cmd);
       let _ = Sys.command cmd in
-      ())
+      () )
 end
 
 module Generate_trees
-    (Store : Irmin.S with type contents = string and type key = string list) =
+    (Store : Irmin.S with type contents = bytes and type key = string list) =
 struct
   let key depth =
     let rec aux i acc =
